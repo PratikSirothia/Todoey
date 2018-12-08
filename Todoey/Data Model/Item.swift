1458dc56b2a628bf00fd6a53f02c54a31c9109c0
Pratik Sirothia <pratik.sirothia@gmail.com>
2018-12-08 11:36:24 +0530

    Model implemented

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Pratik Sirothia on 24/11/18.
+//  Copyright © 2018 Pratik Sirothia. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = true
+    
+}
---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Pratik Sirothia on 24/11/18.
+//  Copyright © 2018 Pratik Sirothia. All rights reserved.
+//
+
+import Foundation
